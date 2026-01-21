ff638eb76ab7091243ef3407e8de6efe82f7787b
Jesscia <suyuchen0920@gmail.com>
2026-01-21 13:22:08 +0800

    second commit-add login component

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,6 @@ const INITIAL_TEMPLATE_DATA = {
 };
 
 function App() {
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-  });
-
   const [isAuth, setIsAuth] = useState(false);
   const [products, setProducts] = useState([]);
   const [templateProduct, setTemplateProduct] = useState(INITIAL_TEMPLATE_DATA);
@@ -135,11 +130,6 @@ function App() {
     }
   };
 
-  // const handleModalImageChange = (index, value) => {
-  //   setTemplateProduct((pre) => {
-  //     const newImages = [...pre.imagesUrl];
-  //     newImages[index] = value;
-
   const handleModalChange = (e) => {
     const { name, value, checked, type } = e.target;
     setTemplateProduct((preData) => ({
@@ -219,30 +209,6 @@ function App() {
     });
   };
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((pre) => ({ ...pre, [name]: value }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const res = await axios.post(`${url}/admin/signin`, formData);
-      const { token, expired } = res.data;
-
-      document.cookie = `hexToken=${token}; expires=${new Date(expired)};`;
-      axios.defaults.headers.common["Authorization"] = token;
-
-      getData();
-      setIsAuth(true);
-    } catch (err) {
-      console.log("完整錯誤:", err.response);
-      alert("登入失敗: " + (err.response?.data?.message || "請檢查帳號密碼"));
-      setIsAuth(false);
-    }
-  };
-
   return (
     <>
       {isAuth ? (
@@ -316,51 +282,7 @@ function App() {
           </div>
         </div>
       ) : (
-        <div className="container login">
-          <div className="row justify-content-center">
-            <h1 className="h3 mb-3 font-weight-normal">請先登入</h1>
-            <div className="col-12">
-              <form
-                id="form"
-                className="form-signin"
-                onSubmit={(e) => handleSubmit(e)}
-              >
-                <div className="form-floating mb-3">
-                  <input
-                    type="email"
-                    className="form-control"
-                    name="username"
-                    placeholder="name@example.com"
-                    value={formData.username}
-                    onChange={(e) => handleInputChange(e)}
-                    required
-                    autoFocus
-                  />
-                  <label htmlFor="username">Email address</label>
-                </div>
-                <div className="form-floating">
-                  <input
-                    type="password"
-                    className="form-control"
-                    name="password"
-                    placeholder="Password"
-                    value={formData.password}
-                    onChange={handleInputChange}
-                    required
-                  />
-                  <label htmlFor="password">Password</label>
-                </div>
-                <button
-                  className="btn btn-lg btn-primary w-100 mt-3"
-                  type="submit"
-                >
-                  登入
-                </button>
-              </form>
-            </div>
-          </div>
-          <p className="mt-5 mb-3 text-muted">&copy; 2024~∞ - 六角學院</p>
-        </div>
+        <Login getData={getData} setIsAuth={setIsAuth} />
       )}
       <ProductModal
         modalType={modalType}
